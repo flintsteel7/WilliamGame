5c63d65aacfddba425087428add73ab676c115b4
Peter Turner <git-commit@flintsteel7.com>
2017-12-13 23:58:37 -0800

    working collision detection

--- characters.lua
+++ characters.lua
@@ -1,49 +1,78 @@
-function create_character()
-  character = {}
-  character.speed = 200
-  character.x = 0
-  character.y = 0
-  character.img = {}
-  character.img.dn = love.graphics.newImage("images/default.png")
-  character.img.up = love.graphics.newImage("images/default.png")
-  character.img.lt = love.graphics.newImage("images/default.png")
-  character.img.rt = love.graphics.newImage("images/default.png")
-  character.img.disp = character.img.dn
-  character.width = character.img.disp:getWidth()
-  character.height = character.img.disp:getHeight()
-  character.hit = {
-    {character.x, character.y},
-    {character.x + character.width, character.y},
-    {character.x, character.y + character.height},
-    {character.x + character.width, character.y + character.height}
+function create_character(x, y)
+  ch = {}
+  ch.speed = 200
+  ch.x = x
+  ch.y = y
+  ch.img = {
+    dn = love.graphics.newImage("images/default.png"),
+    up = love.graphics.newImage("images/default.png"),
+    lt = love.graphics.newImage("images/default.png"),
+    rt = love.graphics.newImage("images/default.png")
   }
-  return character
+  ch.img.disp = ch.img.dn
+  ch.hit_offset = {
+    tp = 0,
+    lt = 0,
+    rt = 0,
+    bt = 0
+  }
+  ch = calculate_hitbox(ch)
+  return ch
 end
 
-function move_character(character, dt)
+function move_character(ch, dt)
+  hit = check_collision(ch, box)
   if love.keyboard.isDown('d') then
-    character.img.disp = character.img.rt
-    character.x = character.x + (character.speed * dt)
+    ch.img.disp = ch.img.rt
+    if not hit.rt then
+      ch.x = ch.x + (ch.speed * dt)
+    end
   end
   if love.keyboard.isDown('a') then
-    character.img.disp = character.img.lt
-    character.x = character.x - (character.speed * dt)
+    ch.img.disp = ch.img.lt
+    if not hit.lt then
+      ch.x = ch.x - (ch.speed * dt)
+    end
   end
   if love.keyboard.isDown('w') then
-    character.img.disp = character.img.up
-    character.y = character.y - (character.speed * dt)
+    ch.img.disp = ch.img.up
+    if not hit.up then
+      ch.y = ch.y - (ch.speed * dt)
+    end
   end
   if love.keyboard.isDown('s') then
-    character.img.disp = character.img.dn
-    character.y = character.y + (character.speed * dt)
+    ch.img.disp = ch.img.dn
+    if not hit.dn then
+      ch.y = ch.y + (ch.speed * dt)
+    end
   end
   -- recalculate hitbox
-  character.width = character.img.disp:getWidth()
-  character.height = character.img.disp:getHeight()
-  character.hit = {
-    {character.x, character.y},
-    {character.x + character.width, character.y},
-    {character.x, character.y + character.height},
-    {character.x + character.width, character.y + character.height}
+  ch = calculate_hitbox(ch)
+
+  return hit
+end
+
+function calculate_hitbox(ch)
+  ch.width = ch.img.disp:getWidth()
+  ch.height = ch.img.disp:getHeight()
+  ch.hit = {
+    xl = ch.x + ch.hit_offset.lt,
+    xr = ch.x + ch.width - ch.hit_offset.rt,
+    yt = ch.y + ch.hit_offset.tp,
+    yb = ch.y + ch.height - ch.hit_offset.bt
+  }
+  return ch
+end
+
+function draw_hitbox_corners(ch)
+  love.graphics.points(ch.hit.xl, ch.hit.yt, ch.hit.xr, ch.hit.yt, ch.hit.xl, ch.hit.yb, ch.hit.xr, ch.hit.yb)
+end
+
+function check_collision(i1, i2)
+  return {
+    dn = i1.hit.yb >= i2.hit.yt and i1.hit.yt < i2.hit.yt and i1.hit.xr >= i2.hit.xl and i1.hit.xl <= i2.hit.xr,
+    up = i1.hit.yt <= i2.hit.yb and i1.hit.yb > i2.hit.yb and i1.hit.xr >= i2.hit.xl and i1.hit.xl <= i2.hit.xr,
+    lt = i1.hit.xl <= i2.hit.xr and i1.hit.xr > i2.hit.xr and i1.hit.yb >= i2.hit.yt and i1.hit.yt <= i2.hit.yb,
+    rt = i1.hit.xr >= i2.hit.xl and i1.hit.xl < i2.hit.xl and i1.hit.yb >= i2.hit.yt and i1.hit.yt <= i2.hit.yb
   }
 end
--- main.lua
+++ main.lua
@@ -2,18 +2,22 @@ require "characters"
 
 dev = true
 
-sam = create_character()
-sam.x = 924
-sam.y = 468
+hit = {}
+
+sam = create_character(924, 468)
 sam.img.dn = love.graphics.newImage("images/sam_dn.png")
 sam.img.up = love.graphics.newImage("images/sam_up.png")
 sam.img.lt = love.graphics.newImage("images/sam_lt.png")
 sam.img.rt = love.graphics.newImage("images/sam_rt.png")
 sam.img.disp = sam.img.dn
+sam.hit_offset = {
+  tp = 60,
+  lt = 6,
+  rt = 6,
+  bt = 0
+}
 
-box = create_character()
-box.x = 650
-box.y = 550
+box = create_character(650, 550)
 
 function love.load()
   love.window.setTitle("William's Game")
@@ -23,18 +27,23 @@ function love.load()
 end
 
 function love.update(dt)
-   move_character(sam, dt)
+  move_character(sam, dt)
 end
 
 function love.draw()
   love.graphics.draw(background, 0, 0)
-  love.graphics.draw(porch, 1300, 400)
-  love.graphics.draw(sam.img.disp, sam.x, sam.y)
+  -- love.graphics.draw(porch, 1300, 400)
   love.graphics.draw(box.img.disp, box.x, box.y)
+  love.graphics.draw(sam.img.disp, sam.x, sam.y)
   if dev then
     love.graphics.print(round(sam.x, 2), 10, 10)
     love.graphics.print(round(sam.y, 2), 70, 10)
-    love.graphics.points(sam.hit[1][1], sam.hit[1][2], sam.hit[2][1], sam.hit[2][2], sam.hit[3][1], sam.hit[3][2], sam.hit[4][1], sam.hit[4][2])
+    love.graphics.print(string.format("hit dn: %s", tostring(hit.dn)), 130, 10)
+    love.graphics.print(string.format("hit up: %s", tostring(hit.up)), 130, 40)
+    love.graphics.print(string.format("hit lt: %s", tostring(hit.lt)), 130, 70)
+    love.graphics.print(string.format("hit rt: %s", tostring(hit.rt)), 130, 100)
+    draw_hitbox_corners(sam)
+    draw_hitbox_corners(box)
   end
 end
 
